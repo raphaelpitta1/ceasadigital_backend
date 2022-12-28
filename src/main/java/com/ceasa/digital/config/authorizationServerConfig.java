c6b0b058aac5e3a6e4ce137a841a645f2d99bd02
raphaelpitta1 <raphael.pitta@hotmail.com>
2022-12-28 16:22:19 -0300

    ajuste

--- src/main/java/com/ceasa/digital/config/authorizationServerConfig.java
+++ src/main/java/com/ceasa/digital/config/authorizationServerConfig.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 
@@ -16,6 +16,7 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableAuthorizationServer
@@ -59,5 +60,12 @@ public class authorizationServerConfig extends AuthorizationServerConfigurerAdap
         clients.inMemory().withClient("99527b18-29c2-4598-94a6-d460e606abf0").secret("8d1e2298-4da8-41d7-92ba-8b99a3152e19").scopes("read","write").authorizedGrantTypes("password").accessTokenValiditySeconds(3600);
 
         
+}
+@Bean
+SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+	http
+		// ...
+		.cors(cors -> cors.disable());
+	return http.build();
 }
 }
--- src/main/java/com/ceasa/digital/config/securityConfig.java
+++ src/main/java/com/ceasa/digital/config/securityConfig.java
@@ -11,8 +11,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -31,7 +33,13 @@ public class securityConfig extends WebSecurityConfigurerAdapter{
 
         auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
-
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+            // ...
+            .cors(cors -> cors.disable());
+        return http.build();
+    }
     @Bean
     public AuthenticationManager authenticationManager() throws Exception{
 
